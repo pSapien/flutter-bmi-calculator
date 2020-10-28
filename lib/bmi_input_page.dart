cc0b5dd4777819e3529170ddb148842ac3cd4379
Prateek Thapa <prateekthapa26@gmail.com>
2020-10-28 21:56:28 +0545

    add card

--- lib/bmi_input_page.dart
+++ lib/bmi_input_page.dart
@@ -11,11 +11,43 @@ class _BMIInputPageState extends State<BMIInputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Calculator'),
+      appBar: AppBar(title: Text('BMI Calculator')),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: BMICard(),
+                ),
+                Expanded(
+                  child: BMICard(),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: BMICard(),
+          ),
+          Expanded(
+            child: BMICard(),
+          ),
+        ],
       ),
-      body: Center(
-        child: Text('Hello World'),
+    );
+  }
+}
+
+class BMICard extends StatelessWidget {
+  const BMICard({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Color(0XFF1D1F33),
       ),
     );
   }
